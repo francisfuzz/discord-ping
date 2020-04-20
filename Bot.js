512b900aafcb00f85c4d9afb50467fd3daf58346
Francis <15894826+francisfuzz@users.noreply.github.com>
2020-04-20 08:33:35 -0700

    Initial commit

---
+++ Bot.js
@@ -0,0 +1,61 @@
+const EventEmitter = require('events');
+const Discord = require('discord.js');
+
+class Bot extends EventEmitter {
+  /**
+   * Initializes all modules, a Discord client, binds events.
+   * @constructor
+   */
+  constructor() {
+    super();
+    this.client = new Discord.Client();
+    this.bindEvents();
+  }
+  
+  /**
+   * Bind event functions.
+   */
+  bindEvents() {
+    this.client.on('ready', this.onReady.bind(this));
+    this.client.on('message', this.onMessage.bind(this));
+  }
+
+  /**
+   * Login client to Discord.
+   */
+  connect() {
+    this.client.login(process.env.AUTH_TOKEN);
+  }
+
+  /**
+   * Destroy Discord client.
+   */
+  destroy() {
+    console.log('Shutting down.');
+    this.client.destroy();
+  }
+
+  /**
+   * Bot is connected to Discord.
+   */
+  onReady() {
+    console.log(`Connected to Discord as ${this.client.user.username}#${this.client.user.discriminator} <@${this.client.user.id}>`);
+  }
+
+  /**
+   * Handles messages.
+   * @param {Message} Message Discord message object.
+   */
+  onMessage(Message) {
+    // Ignore system, bot messages
+    if (Message.system || Message.author.bot) {
+      return;
+    }
+
+    if (Message.content === 'ping') {
+      Message.reply('pong 🏓');
+    }
+  }
+}
+
+module.exports = Bot;
